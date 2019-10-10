5fbef473f5ee481613981300eb762196c2230382
Zsolt Uveges <uveges.zsolt@r-szoft.hu>
2019-10-10 10:37:26 +0200

    Implement Cities and City models

--- src/models/Cities.java
+++ src/models/Cities.java
@@ -1,4 +1,22 @@
 package models;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+@XmlRootElement(name = "cities")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Cities {
+
+    @XmlElement(name = "city")
+    private ArrayList<City> cities;
+
+    public Cities() {
+    }
+
+    public ArrayList<City> getCities() {
+        return cities;
+    }
 }
--- src/models/City.java
+++ src/models/City.java
@@ -1,4 +1,49 @@
 package models;
 
 public class City {
+    private int id;
+    private String name;
+    private int population;
+
+    public City() {
+    }
+
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", numberOfPeople=" + population +
+                '}';
+    }
 }
