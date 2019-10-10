a8c57bbfc8f3b2e5d5bfd2affb5174c451ee4014
Zsolt Uveges <uveges.zsolt@r-szoft.hu>
2019-10-10 10:42:15 +0200

    Implement FlightService

--- src/services/FlightService.java
+++ src/services/FlightService.java
@@ -1,4 +1,48 @@
 package services;
 
-public class FlightService {
+import models.Flight;
+import models.Flights;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.ArrayList;
+
+public class FlightService implements XmlReader {
+    private File flightsFile;
+    private JAXBContext jaxbContext;
+    private Unmarshaller jaxbUnmarshaller;
+    private String filePath = "resources/flights.xml";
+    private ArrayList<Flight> flights;
+    private AirlineService airlineService;
+    private CityService cityService;
+
+    public FlightService(AirlineService airlineService, CityService cityService) {
+        this.airlineService = airlineService;
+        this.cityService = cityService;
+        try {
+            initParser();
+            readDataFromFile();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    @Override
+    public void initParser() throws JAXBException {
+        flightsFile = new File(filePath);
+        jaxbContext = JAXBContext.newInstance(Flights.class);
+        jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+    }
+
+    @Override
+    public void readDataFromFile() throws JAXBException {
+        Flights flightsModel = (Flights) jaxbUnmarshaller.unmarshal(flightsFile);
+        flights = flightsModel.getFlights();
+    }
 }
