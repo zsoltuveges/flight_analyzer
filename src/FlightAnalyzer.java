280ae8264d45df69826f84a88b63488952004734
Zsolt Uveges <uveges.zsolt@r-szoft.hu>
2019-10-10 10:45:36 +0200

    Inject services to the analyzer

--- src/FlightAnalyzer.java
+++ src/FlightAnalyzer.java
@@ -1,5 +1,16 @@
+import services.AirlineService;
+import services.CityService;
+import services.FlightService;
+
 public class FlightAnalyzer {
+    private AirlineService airlineService;
+    private FlightService flightService;
+    private CityService cityService;
+
     public FlightAnalyzer() {
+        airlineService = new AirlineService();
+        cityService = new CityService();
+        flightService = new FlightService(airlineService, cityService);
     }
 
     public void start() {
