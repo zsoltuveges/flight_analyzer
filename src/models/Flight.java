592fd237663df28f56e92b4df17f3ec9dafb4deb
Zsolt Uveges <uveges.zsolt@r-szoft.hu>
2019-10-10 10:41:04 +0200

    Implement Flights and Flight model

--- src/models/Flight.java
+++ src/models/Flight.java
@@ -1,4 +1,62 @@
 package models;
 
 public class Flight {
+    private int id;
+    private int distance;
+    private int timeInterval;
+    private Airline airline;
+    private City origin;
+    private City destination;
+
+    public Flight() {
+    }
+
+    public Flight(int id, int distance, int timeInterval, Airline airline, City origin, City destination) {
+        this.id = id;
+        this.distance = distance;
+        this.timeInterval = timeInterval;
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTimeInterval() {
+        return timeInterval;
+    }
+
+    public void setTimeInterval(int timeInterval) {
+        this.timeInterval = timeInterval;
+    }
+
+    public Airline getAirline() {
+        return airline;
+    }
+
+    public void setAirline(Airline airline) {
+        this.airline = airline;
+    }
+
+    public City getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(City origin) {
+        this.origin = origin;
+    }
+
+    public City getDestination() {
+        return destination;
+    }
+
+    public void setDestination(City destination) {
+        this.destination = destination;
+    }
 }
--- src/models/Flights.java
+++ src/models/Flights.java
@@ -1,4 +1,26 @@
 package models;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+@XmlRootElement(name = "flights")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Flights {
+
+    @XmlElement(name = "flight")
+    private ArrayList<Flight> flights;
+
+    public Flights() {
+    }
+
+    public ArrayList<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(ArrayList<Flight> flights) {
+        this.flights = flights;
+    }
 }
